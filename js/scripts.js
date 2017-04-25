62714ca3b3684b955e438415252700b980e4f6a5
Your Name <Cacek@users.noreply.github.com>
2017-04-26 00:57:20 +0200

    Array Functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
+var allNames = femaleNames.concat(maleNames);
+
+function getNewName(name) {
+    var name = prompt('Podaj imie jakie chcesz dodac');
+    if (allNames.includes(name)) {
+        alert('Podane imie znajduje sie juz w bazie');
+    } else {
+        allNames.push(name);
+        return document.write('Dodales imie ' + name + ' do bazy' + '<BR>');
+    }
+}
+
+function printArray(array) {
+    for (i = 0; i < array.length; i++) {
+        document.write(array[i] + '<BR>');
+    }
+}
+
+
+
+
+document.write(getNewName(allNames));
+
+document.write('Baza aktualnie zawiera następujące imiona: ');
+document.write(printArray(allNames));
