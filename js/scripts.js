17c679a795c855d85dec8f21bec56c78dad6243e
Your Name <Cacek@users.noreply.github.com>
2017-04-25 23:16:05 +0200

    Add function getTriangleArea();

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+var a = prompt('Podaj a: ');
+var h = prompt('Podaj h: ');
 
+function getTriangleArea(a, h) {
+    if ((a <= 0) && (h <= 0)) {
+        document.writeln('Podales nieprawidlowe dane, wszystkie musza być większe od 0');
+    } else {
+        
+        return  a * h / 2;
+    }
+}
+
+document.writeln('Zmienna A: ' + a + '<BR>');
+document.writeln('Zmienna H: ' + h + '<BR>');
+document.writeln('Wynik :' + getTriangleArea(a,h)) ;
