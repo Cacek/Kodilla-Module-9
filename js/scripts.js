5cf38209fb82f0f930d9c5b76cac7869951e291c
Cacek <you@example.com>
2017-04-26 12:19:49 +0200

    String functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+var animal = "Zielone słonie";
+var animal = animal.toUpperCase();
+var textUpdate = text.replace('Papugi', animal);
 
+document.write(textUpdate);
+
+var textSliced = textUpdate.slice(0, textUpdate.length / 2);
+document.write('<BR>' + textSliced);
