d4401c1541d6009959dd80b0926bdb56b617c2af
Your Name <Cacek@users.noreply.github.com>
2017-04-29 21:11:56 +0200

    Zadanie_9.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
+var tree = prompt('Podaj wysokosc choinki: ');
+
+function drawTree(tree) {
+    for (var i = 1; i <= tree; i++) 
+    {
+        for (var j = i; j < tree; j++) {
+            console.log(' ');
+        }
+
+        for (var j = 1; j <= (2 * i - 1); j++) {
+            console.log('*');
+        }
+
+    console.log('\n');
+    }
+
+}
+
+console.log(drawTree(tree));
+
+
 
