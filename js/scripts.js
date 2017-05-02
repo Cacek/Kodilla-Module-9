d131c25119f73ec09636817ca152414a0d8950ab
Your Name <Cacek@users.noreply.github.com>
2017-05-03 00:34:54 +0200

    Zadanie 9.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+    var list = document.getElementById('list');
+    var add = document.getElementById('btn-addElem');
+    
+    add.addEventListener('click', function() {
+        var x = document.getElementsByTagName('li').length;
+        list.innerHTML += "<li>item</li>" + x;
+        console.log('przycisk klikniety'); 
+    })
 
